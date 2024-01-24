eb9e44b6a07c93d507181d07080a9079c78164ff
JoseDelgado1899 <delgadojose213@gmail.com>
2024-01-23 20:19:17 -0500

    n

---
+++ Python_AndresCastellanos/introduccion.py
@@ -0,0 +1,12 @@
+# Introduccion en Python
+
+numero = 10
+print(type(numero))
+palabra = "ejemplo"
+print(type(palabra))
+condicion = True
+print(type(condicion))
+numReal = 2.5
+print(type(numReal))
+numCompx = 3 + 4j
+print(type(numCompx))
