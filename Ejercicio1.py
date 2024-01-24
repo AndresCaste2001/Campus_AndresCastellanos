077d05c3314654084abb578235963744eb4952b2
JoseDelgado1899 <delgadojose213@gmail.com>
2024-01-23 20:23:16 -0500

    segundo ejercicio python

--- Ejercicio1.py
+++
@@ -1,1 +0,0 @@
-#Mi primer comentario en pythonsito<3
---
+++ Operaciones.py
@@ -0,0 +1,11 @@
+a = 4
+b = 6
+
+suma = a+b
+multiplicacion = a*b
+division = a/b
+potencia = b**4
+modulo = a%b
+divisionN = 6/3
+
+print(suma, " ", multiplicacion," ",division, " ",potencia, " ", modulo, " ", divisionN  )
