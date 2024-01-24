87a3accf9565f793e19e89839a7a84264c48337e
AndresCaste2001 <143757955+AndresCaste2001@users.noreply.github.com>
2024-01-23 21:46:21 -0500

    Create segundoPuntoClase.py

---
+++ Python_AndresCastellanos/segundoPuntoClase.py
@@ -0,0 +1,11 @@
+notas = input("Dame tu nota: \n")
+notas = int(notas)
+
+if notas>=90:
+    print("Tu nota es A")
+elif notas>=80:
+    print("Tu nota es B")
+elif notas>=70:
+    print("Tu nota es C")
+else:
+    print("Tu nota es F")
